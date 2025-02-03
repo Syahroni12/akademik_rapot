73c6a81a7a1f2a0bc8a375074ab1f2bbd6f70952
Syahroni12 <ronixria@gmail.com>
2025-02-03 08:40:12 +0700

    first commit awal awal

---
+++ resources/views/jurusan.blade.php
@@ -0,0 +1,26 @@
+@extends('templating.main')
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Jurusan</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
